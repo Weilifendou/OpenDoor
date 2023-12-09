defa335191a5e485a3aa4a82803e868f820c581d
suhangdd <hql4072@qq.com>
2023-12-10 01:28:46 +0800

    fix bug

---
+++ STM32Downloader_20231209/Template/Form1.Events.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Template
+{
+    public partial class Form1 : Form
+    {
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Array.Clear(SppReceiveBuff, 0, SppReceiveBuff.Length);
+            int length = serialPort.BytesToRead;
+            byte[] buffer = new byte[length];
+            serialPort.Read(buffer, 0, length);
+            serialPort.DiscardInBuffer();
+            SppReceiveBuff = buffer;
+            BeginInvoke(new Action(() =>
+            {
+                if (checkBoxReceive.Checked)
+                {
+                    if (radioButtonRecAbc.Checked)
+                    {
+                        textBoxReceive.AppendText(Encoding.Default.GetString(SppReceiveBuff));
+                        if (checkBoxNewLine.Checked)
+                        {
+                            textBoxReceive.AppendText("\r\n");
+                        }
+                    }
+                    else if (radioButtonRecHex.Checked)
+                    {
+                        foreach (byte b in SppReceiveBuff)
+                        {
+                            textBoxReceive.AppendText(b.ToString("X2") + " ");
+                        }
+                    }
+                }
+            }));
+        }
+        private void Ble_DeviceReceiveData(byte[] data)
+        {
+            Array.Clear(BleReceiveBuff, 0, BleReceiveBuff.Length);
+            BleReceiveBuff = data;
+            BeginInvoke(new Action(() =>
+            {
+                if (checkBoxReceive.Checked)
+                {
+                    if (radioButtonRecAbc.Checked)
+                    {
+                        textBoxReceive.AppendText(Encoding.Default.GetString(data));
+                        if (checkBoxNewLine.Checked)
+                        {
+                            textBoxReceive.AppendText("\r\n");
+                        }
+                    }
+                    else if (radioButtonRecHex.Checked)
+                    {
+                        foreach (byte b in data)
+                        {
+                            textBoxReceive.AppendText(b.ToString("X2") + " ");
+                        }
+                    }
+                }
+            }));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ble.StartBleDeviceWatcher();
+        }
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "BIN文件|*.bin";
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FilePath = dialog.FileName;
+                textBoxFilePath.Text = FilePath;
+            }
+
+        }
+
+        private void buttonOperate_Click(object sender, EventArgs e)
+        {
+            if (radioButtonSpp.Checked)
+            {
+                OperateSpp();
+            }
+            else if (radioButtonBle.Checked)
+            {
+                OperateBle();
+            }
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            if (FilePath == null) return;
+            buttonDownload.Enabled = false;
+            buttonOperate.Enabled = false;
+            buttonResetSpp.Enabled = false;
+            buttonResetBle.Enabled = false;
+            FileLength = 0;
+            Progress = 0;
+            if (radioButtonSpp.Checked)
+            {
+                SendFileFrame(SppReadFrame, SppFrame);
+            }
+            else if (radioButtonBle.Checked)
+            {
+                SendFileFrame(BleReadFrame, BleFrame);
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ble.DeviceDispose();
+            SendThread?.Abort();
+            TimerThread?.Abort();
+            Environment.Exit(0);
+        }
+
+        private void checkBoxTopMost_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            byte[] buff = null;
+            string text = textBoxSend.Text;
+            if (radioButtonSendAbc.Checked)
+            {
+                buff = Encoding.Default.GetBytes(text);
+            }
+            else if (radioButtonSendHex.Checked)
+            {
+                buff = HexStringToByte(text);
+            }
+            if (radioButtonSpp.Checked)
+            {
+                serialPort.Write(buff, 0, buff.Length);
+            }
+            else if (radioButtonBle.Checked)
+            {
+                ble.Write(buff);
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            ble.StartBleDeviceWatcher();
+        }
+
+        private void buttonClearReceive_Click(object sender, EventArgs e)
+        {
+            textBoxReceive.Text = "";
+        }
+
+        private void buttonClearSend_Click(object sender, EventArgs e)
+        {
+            textBoxSend.Text = "";
+        }
+
+        private void radioButtonSpp_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxPortList.Enabled = radioButtonSpp.Checked;
+            comboBoxDeviceList.Enabled = radioButtonBle.Checked;
+        }
+
+        private void radioButtonBle_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxPortList.Enabled = radioButtonSpp.Checked;
+            comboBoxDeviceList.Enabled = radioButtonBle.Checked;
+        }
+
+        private void buttonResetSpp_Click(object sender, EventArgs e)
+        {
+            if (radioButtonSpp.Checked)
+            {
+                SendData(new byte[0], SppSendBuff, 0x55, true);
+            }
+            else if (radioButtonBle.Checked)
+            {
+                SendData(new byte[0], BleSendBuff, 0x55, true);
+            }
+
+        }
+
+        private void buttonResetBle_Click(object sender, EventArgs e)
+        {
+            if (radioButtonSpp.Checked)
+            {
+                SendData(new byte[0], SppSendBuff, 0xaa, true);
+            }
+            else if (radioButtonBle.Checked)
+            {
+                SendData(new byte[0], BleSendBuff, 0xaa, true);
+            }
+
+        }
+
+        private void radioButtonRecAbc_CheckedChanged(object sender, EventArgs e)
+        {
+            Transform(radioButtonRecAbc.Checked, radioButtonRecHex.Checked, textBoxReceive);
+
+        }
+        private void radioButtonRecHex_CheckedChanged(object sender, EventArgs e)
+        {
+            Transform(radioButtonRecAbc.Checked, radioButtonRecHex.Checked, textBoxReceive);
+
+        }
+
+        private void radioButtonSendAbc_CheckedChanged(object sender, EventArgs e)
+        {
+            Transform(radioButtonSendAbc.Checked, radioButtonSendHex.Checked, textBoxSend);
+        }
+
+        private void radioButtonSendHex_CheckedChanged(object sender, EventArgs e)
+        {
+            Transform(radioButtonSendAbc.Checked, radioButtonSendHex.Checked, textBoxSend);
+        }
+    }
+}
--- STM32Downloader_20231209/Template/Form1.cs
+++ STM32Downloader_20231209/Template/Form1.cs
@@ -14,7 +14,6 @@ namespace Template
 {
     public partial class Form1 : Form
     {
-
         private BleCore ble;
         private delegate void RenewInterfaceHandler();
         private const int SppFrame = 1024;
@@ -83,60 +82,6 @@ namespace Template
             }));
         }
 
-        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            Array.Clear(SppReceiveBuff, 0, SppReceiveBuff.Length);
-            int length = serialPort.BytesToRead;
-            byte[] buffer = new byte[length];
-            serialPort.Read(buffer, 0, length);
-            serialPort.DiscardInBuffer();
-            SppReceiveBuff = buffer;
-            BeginInvoke(new Action(() =>
-            {
-                if (checkBoxReceive.Checked)
-                {
-                    if (radioButtonRecAbc.Checked)
-                    {
-                        textBoxReceive.AppendText(Encoding.Default.GetString(SppReceiveBuff));
-                        if (checkBoxNewLine.Checked)
-                        {
-                            textBoxReceive.AppendText("\r\n");
-                        }
-                    }
-                    else if (radioButtonRecHex.Checked)
-                    {
-                        foreach(byte b in SppReceiveBuff)
-                        {
-                            textBoxReceive.AppendText(b.ToString("X2") + " ");
-                        }
-                    }
-                }
-            }));
-        }
-        private void Ble_DeviceReceiveData(byte[] data)
-        {
-            Array.Clear(BleReceiveBuff, 0, BleReceiveBuff.Length);
-            BleReceiveBuff = data;
-            BeginInvoke(new Action(() =>
-            {
-                if (radioButtonRecAbc.Checked)
-                {
-
-                    textBoxReceive.AppendText(Encoding.Default.GetString(data));
-                    if (checkBoxNewLine.Checked)
-                    {
-                        textBoxReceive.AppendText("\r\n");
-                    }
-                }
-                else if (radioButtonRecHex.Checked)
-                {
-                    foreach (byte b in data)
-                    {
-                        textBoxReceive.AppendText(b.ToString("X2") + " ");
-                    }
-                }
-            }));
-        }
 
         private void Ble_DeviceWatcherChanged(BluetoothLEDevice bluetoothLEDevice)
         {
@@ -146,12 +91,7 @@ namespace Template
             }));
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            ble.StartBleDeviceWatcher();
-        }
-
-        private void SendData(byte[] dat, byte[] sendBuff, int funCode, bool flag)
+        private void SendData(byte[] dat, byte[] sendBuff, int funCode, bool type)
         {
             int s = 0;
             int header = 7;
@@ -165,7 +105,7 @@ namespace Template
             sendBuff[s++] = (byte)funCode;
             for (int i = 0; i < dat.Length; i++)
                 sendBuff[s++] = dat[i];
-            if (flag)
+            if (type)
             {
                 int crc = CRCCheckout(sendBuff, datLength);
                 sendBuff[s++] = (byte)(crc >> 8);
@@ -215,9 +155,12 @@ namespace Template
         {
             if (Overtime < 2)
             {
+                var percent = Progress * 100.0 / FileLength;
                 if (Progress >= FileLength)
                 {
                     Progress = FileLength;
+                    labelState.Text = "写入完成，进度为 " + percent.ToString("00.00") + "%";
+                    myProgressBar.Progress = (int)percent;
                     labelState.Text = "写入完成";
                     buttonDownload.Text = "写入固件";
                     buttonDownload.Enabled = true;
@@ -225,9 +168,12 @@ namespace Template
                     buttonResetSpp.Enabled = true;
                     buttonResetBle.Enabled = true;
                 }
-                var percent = Progress * 100.0 / FileLength;
-                labelState.Text = "正在写入，进度为 " + percent.ToString("00.00") + "%";
-                myProgressBar.Progress = (int)percent;
+                else
+                {
+                    buttonDownload.Text = "正在写入";
+                    labelState.Text = "正在写入，进度为 " + percent.ToString("00.00") + "%";
+                    myProgressBar.Progress = (int)percent;
+                }
             }
             else if (Overtime < 6)
             {
@@ -244,19 +190,6 @@ namespace Template
             }
 
         }
-        private void buttonLoadFile_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "BIN文件|*.bin";
-            DialogResult result = dialog.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                FilePath = dialog.FileName;
-                textBoxFilePath.Text = FilePath;
-            }
-
-        }
-
         private void OperateSpp()
         {
             try
@@ -299,6 +232,7 @@ namespace Template
                 var device = ble.deviceList.Where(u => u.Name == deviceName).FirstOrDefault();
                 ble.StopBleDeviceWathcer();
                 ble.FindService(device);
+                comboBoxDeviceList.Enabled = false;
             }
             else
             {
@@ -306,19 +240,7 @@ namespace Template
                 comboBoxDeviceList.Enabled = true;
             }
         }
-        private void buttonOperate_Click(object sender, EventArgs e)
-        {
-            if (radioButtonSpp.Checked)
-            {
-                OperateSpp();
-            }
-            else if (radioButtonBle.Checked)
-            {
-                OperateBle();
-            }
-        }
-
-        private void DownloadFile(byte[] readFrame, int frameSize)
+        private void SendFileFrame(byte[] readFrame, int frameSize)
         {
             SendThread = new Thread(new ThreadStart(() =>
             {
@@ -419,121 +341,11 @@ namespace Template
                         SendData(new byte[0], BleSendBuff, 3, false);
                     }
                     TimerThread.Abort();
-
                 }
             }));
             SendThread.Start();
 
         }
-        private void buttonDownload_Click(object sender, EventArgs e)
-        {
-            if (FilePath == null) return;
-            buttonDownload.Enabled = false;
-            buttonOperate.Enabled = false;
-            buttonResetSpp.Enabled = false;
-            buttonResetBle.Enabled = false;
-            buttonDownload.Text = "正在写入";
-            FileLength = 0;
-            Progress = 0;
-            if (radioButtonSpp.Checked)
-            {
-                DownloadFile(SppReadFrame, SppFrame);
-            }
-            else if (radioButtonBle.Checked)
-            {
-                DownloadFile(BleReadFrame, BleFrame);
-            }
-
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            ble.DeviceDispose();
-            SendThread?.Abort();
-            TimerThread?.Abort();
-            Environment.Exit(0);
-        }
-
-        private void checkBoxTopMost_CheckedChanged(object sender, EventArgs e)
-        {
-            TopMost = !TopMost;
-        }
-
-        private void buttonSend_Click(object sender, EventArgs e)
-        {
-            byte[] buff = null;
-            string text = textBoxSend.Text;
-            if (radioButtonSendAbc.Checked)
-            {
-                buff = Encoding.Default.GetBytes(text);
-            }
-            else if (radioButtonSendHex.Checked)
-            {
-                buff = HexStringToByte(text);
-            }
-            if (radioButtonSpp.Checked)
-            {
-                serialPort.Write(buff, 0, buff.Length);
-            }
-            else if (radioButtonBle.Checked)
-            {
-                ble.Write(buff);
-            }
-        }
-
-        private void buttonSearch_Click(object sender, EventArgs e)
-        {
-            ble.StartBleDeviceWatcher();
-        }
-
-        private void buttonClearReceive_Click(object sender, EventArgs e)
-        {
-            textBoxReceive.Text = "";
-        }
-
-        private void buttonClearSend_Click(object sender, EventArgs e)
-        {
-            textBoxSend.Text = "";
-        }
-
-        private void radioButtonSpp_CheckedChanged(object sender, EventArgs e)
-        {
-            comboBoxPortList.Enabled = radioButtonSpp.Checked;
-            comboBoxDeviceList.Enabled = radioButtonBle.Checked;
-        }
-
-        private void radioButtonBle_CheckedChanged(object sender, EventArgs e)
-        {
-            comboBoxPortList.Enabled = radioButtonSpp.Checked;
-            comboBoxDeviceList.Enabled = radioButtonBle.Checked;
-        }
-
-        private void buttonResetSpp_Click(object sender, EventArgs e)
-        {
-            if (radioButtonSpp.Checked)
-            {
-                SendData(new byte[0], SppSendBuff, 0x55, true);
-            }
-            else if (radioButtonBle.Checked)
-            {
-                SendData(new byte[0], BleSendBuff, 0x55, true);
-            }
-
-        }
-
-        private void buttonResetBle_Click(object sender, EventArgs e)
-        {
-            if (radioButtonSpp.Checked)
-            {
-                SendData(new byte[0], SppSendBuff, 0xaa, true);
-            }
-            else if (radioButtonBle.Checked)
-            {
-                SendData(new byte[0], BleSendBuff, 0xaa, true);
-            }
-
-        }
-
         private byte[] HexStringToByte(string hex)
         {
             try
@@ -564,69 +376,21 @@ namespace Template
             return s;
 
         }
-        private void radioButtonRecAbc_CheckedChanged(object sender, EventArgs e)
+        private void Transform(bool check1, bool check2, TextBox textbox)
         {
-            if (radioButtonRecAbc.Checked)
+            if (check1)
             {
-                string hex = textBoxReceive.Text;
-                textBoxReceive.Text = "";
+                string hex = textbox.Text;
+                textbox.Text = "";
                 byte[] b = HexStringToByte(hex);
-                textBoxReceive.Text = Encoding.Default.GetString(b);
+                textbox.Text = Encoding.Default.GetString(b);
             }
-            else if (radioButtonRecHex.Checked)
+            else if (check2)
             {
-                string abc = textBoxReceive.Text;
-                textBoxReceive.Text = AbcStringToByteString(abc);
+                string abc = textbox.Text;
+                textbox.Text = AbcStringToByteString(abc);
             }
 
-        }
-        private void radioButtonRecHex_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButtonRecAbc.Checked)
-            {
-                string hex = textBoxReceive.Text;
-                textBoxReceive.Text = "";
-                byte[] b = HexStringToByte(hex);
-                textBoxReceive.Text = Encoding.Default.GetString(b);
-            }
-            else if (radioButtonRecHex.Checked)
-            {
-                string abc = textBoxReceive.Text;
-                textBoxReceive.Text = AbcStringToByteString(abc);
-            }
-
-        }
-
-        private void radioButtonSendAbc_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButtonSendAbc.Checked)
-            {
-                string hex = textBoxSend.Text;
-                textBoxSend.Text = "";
-                byte[] b = HexStringToByte(hex);
-                textBoxSend.Text = Encoding.Default.GetString(b);
-            }
-            else if (radioButtonSendHex.Checked)
-            {
-                string abc = textBoxSend.Text;
-                textBoxSend.Text = AbcStringToByteString(abc);
-            }
-        }
-
-        private void radioButtonSendHex_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButtonSendAbc.Checked)
-            {
-                string hex = textBoxSend.Text;
-                textBoxSend.Text = "";
-                byte[] b = HexStringToByte(hex);
-                textBoxSend.Text = Encoding.Default.GetString(b);
-            }
-            else if (radioButtonSendHex.Checked)
-            {
-                string abc = textBoxSend.Text;
-                textBoxSend.Text = AbcStringToByteString(abc);
-            }
         }
     }
 }
